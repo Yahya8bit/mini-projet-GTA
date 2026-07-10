f4906f757c7e620a15200bb88c1f623fde52d556
Yahya8bit <mohamedyahia.gazzah@ensi-uma.tn>
2026-07-10 12:19:47 +0100

    fixed dbsqlite bug in first docker mount

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -78,12 +78,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "data" / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
